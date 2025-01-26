6d1d9e94411dd4afedb7b4b1e3fa257c39d66aac
WFPhantom <wfphantom5@yahoo.com>
2025-01-26 09:17:07 +0200

    1.21.1 setup

---
+++ kubejs/client_scripts/main.js
@@ -0,0 +1,17 @@
+// Visit the wiki for more info - https://kubejs.com/
+RecipeViewerEvents.removeEntries('item', event => {
+    event.remove('minecraft:crafter'),
+    event.remove('aquaculture:neptunium_helmet'),
+    event.remove('aquaculture:neptunium_chestplate'),
+    event.remove('aquaculture:neptunium_leggings'),
+    event.remove('aquaculture:neptunium_boots')
+    event.remove('aquaculture:neptunium_pickaxe')
+    event.remove('aquaculture:neptunium_axe')
+    event.remove('aquaculture:neptunium_shovel')
+    event.remove('aquaculture:neptunium_sword')
+    event.remove('aquaculture:neptunium_bow')  
+    event.remove('aquaculture:neptunium_hoe')  
+    event.remove('redstonepen:remote') 
+    event.remove('redstonepen:basic_gauge')
+    event.remove('minecraft:spyglass')
+})
---
+++ kubejs/server_scripts/main.js
@@ -0,0 +1,39 @@
+// Visit the wiki for more info - https://kubejs.com/
+RecipeViewerEvents.removeRecipes(event => {
+    event.remove('minecraft:crafter'),
+    event.remove('aquaculture:neptunium_helmet'),
+    event.remove('aquaculture:neptunium_chestplate'),
+    event.remove('aquaculture:neptunium_leggings'),
+    event.remove('aquaculture:neptunium_boots')
+    event.remove('aquaculture:neptunium_pickaxe')
+    event.remove('aquaculture:neptunium_axe')
+    event.remove('aquaculture:neptunium_shovel')
+    event.remove('aquaculture:neptunium_sword')
+    event.remove('aquaculture:neptunium_bow')  
+    event.remove('aquaculture:neptunium_hoe')  
+    event.remove('redstonepen:remote') 
+    event.remove('redstonepen:basic_gauge')
+    event.remove('minecraft:spyglass')
+})
+
+ServerEvents.recipes(event => {
+    event.remove({output: 'minecraft:crafter'}),
+    event.remove({output: 'aquaculture:neptunium_helmet'}),
+    event.remove({output: 'aquaculture:neptunium_chestplate'}),
+    event.remove({output: 'aquaculture:neptunium_leggings'}),
+    event.remove({output: 'aquaculture:neptunium_boots'})
+    event.remove({output: 'aquaculture:neptunium_pickaxe'})
+    event.remove({output: 'aquaculture:neptunium_axe'})
+    event.remove({output: 'aquaculture:neptunium_shovel'})
+    event.remove({output: 'aquaculture:neptunium_sword'})
+    event.remove({output: 'aquaculture:neptunium_bow'})
+    event.remove({output: 'aquaculture:neptunium_hoe'})
+    event.remove({output: 'redstonepen:remote'})
+    event.remove({output: 'redstonepen:basic_gauge'})
+    event.remove({output: 'minecraft:spyglass'})
+})
+
+
+ServerEvents.tags("worldgen/biome", (event) => {
+    event.get("ae2:has_meteorites").removeAll()
+  })
