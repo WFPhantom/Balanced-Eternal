0452eb78878a1cee6989957d7d52306a4fa8afb2
WFPhantom <wfphantom5@yahoo.com>
2025-02-14 10:04:40 +0200

    Remove MVS, MES, MNS, Nature's Compass, Structory: Towers, Repurposed Structures, Aquaculture and Alternate Current, updates, Sort KubeJS scripts, Replace REI with EMI

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -1,63 +1,43 @@
 // Visit the wiki for more info - https://kubejs.com/
-RecipeViewerEvents.removeRecipes(event => {
-    event.remove('minecraft:crafter'),
-    event.remove('aquaculture:neptunium_helmet'),
-    event.remove('aquaculture:neptunium_chestplate'),
-    event.remove('aquaculture:neptunium_leggings'),
-    event.remove('aquaculture:neptunium_boots')
-    event.remove('aquaculture:neptunium_pickaxe')
-    event.remove('aquaculture:neptunium_axe')
-    event.remove('aquaculture:neptunium_shovel')
-    event.remove('aquaculture:neptunium_sword')
-    event.remove('aquaculture:neptunium_bow')  
-    event.remove('aquaculture:neptunium_hoe')  
-    event.remove('redstonepen:remote') 
-    event.remove('redstonepen:basic_gauge')
-    event.remove('minecraft:spyglass')
-    event.remove('computercraft:computer_command')
-    event.remove('ae2:meteorite_compass')
-    event.remove('ae2:guide')
-    event.remove('ae2:matter_cannon')
-    event.remove('ae2:portable_item_cell_1k')
-    event.remove('ae2:portable_item_cell_4k')
-    event.remove('ae2:portable_item_cell_16k')
-    event.remove('ae2:portable_item_cell_64k')
-    event.remove('ae2:portable_item_cell_256k')
-    event.remove('ae2:portable_fluid_cell_1k')
-    event.remove('ae2:portable_fluid_cell_4k')
-    event.remove('ae2:portable_fluid_cell_16k')
-    event.remove('ae2:portable_fluid_cell_64k')
-    event.remove('ae2:portable_fluid_cell_256k')
-})
-
 ServerEvents.recipes(event => {
+    // MINECRAFT
     event.remove({output: 'minecraft:crafter'}),
+    event.remove({input: 'minecraft:crafter'}),
+    event.remove({output: 'minecraft:spyglass'}),
+
+    // AQUACULTURE
     event.remove({output: 'aquaculture:neptunium_helmet'}),
     event.remove({output: 'aquaculture:neptunium_chestplate'}),
     event.remove({output: 'aquaculture:neptunium_leggings'}),
-    event.remove({output: 'aquaculture:neptunium_boots'})
-    event.remove({output: 'aquaculture:neptunium_pickaxe'})
-    event.remove({output: 'aquaculture:neptunium_axe'})
-    event.remove({output: 'aquaculture:neptunium_shovel'})
-    event.remove({output: 'aquaculture:neptunium_sword'})
-    event.remove({output: 'aquaculture:neptunium_bow'})
-    event.remove({output: 'aquaculture:neptunium_hoe'})
-    event.remove({output: 'redstonepen:remote'})
-    event.remove({output: 'redstonepen:basic_gauge'})
-    event.remove({output: 'minecraft:spyglass'})
-    event.remove({output: 'computercraft:computer_command'})
-    event.remove({output: 'ae2:meteorite_compass'})
-    event.remove({output: 'ae2:guide'})
-    event.remove({output: 'ae2:matter_cannon'})
-    event.remove({output: 'ae2:portable_item_cell_1k'})
-    event.remove({output: 'ae2:portable_item_cell_4k'})
-    event.remove({output: 'ae2:portable_item_cell_16k'})
-    event.remove({output: 'ae2:portable_item_cell_64k'})
-    event.remove({output: 'ae2:portable_item_cell_256k'})
-    event.remove({output: 'ae2:portable_fluid_cell_1k'})
-    event.remove({output: 'ae2:portable_fluid_cell_4k'})
-    event.remove({output: 'ae2:portable_fluid_cell_16k'})
-    event.remove({output: 'ae2:portable_fluid_cell_64k'})
+    event.remove({output: 'aquaculture:neptunium_boots'}),
+    event.remove({output: 'aquaculture:neptunium_pickaxe'}),
+    event.remove({output: 'aquaculture:neptunium_axe'}),
+    event.remove({output: 'aquaculture:neptunium_shovel'}),
+    event.remove({output: 'aquaculture:neptunium_sword'}),
+    event.remove({output: 'aquaculture:neptunium_bow'}),
+    event.remove({output: 'aquaculture:neptunium_hoe'}),
+    event.remove({output: 'aquaculture:neptunes_bounty'}),
+
+    // REDSTONE PEN
+    event.remove({output: 'redstonepen:remote'}),
+    event.remove({output: 'redstonepen:basic_gauge'}),
+
+    // COMPUTERCRAFT
+    event.remove({output: 'computercraft:computer_command'}),
+
+    // AE2
+    event.remove({output: 'ae2:meteorite_compass'}),
+    event.remove({output: 'ae2:guide'}),
+    event.remove({output: 'ae2:matter_cannon'}),
+    event.remove({output: 'ae2:portable_item_cell_1k'}),
+    event.remove({output: 'ae2:portable_item_cell_4k'}),
+    event.remove({output: 'ae2:portable_item_cell_16k'}),
+    event.remove({output: 'ae2:portable_item_cell_64k'}),
+    event.remove({output: 'ae2:portable_item_cell_256k'}),
+    event.remove({output: 'ae2:portable_fluid_cell_1k'}),
+    event.remove({output: 'ae2:portable_fluid_cell_4k'}),
+    event.remove({output: 'ae2:portable_fluid_cell_16k'}),
+    event.remove({output: 'ae2:portable_fluid_cell_64k'}),
     event.remove({output: 'ae2:portable_fluid_cell_256k'})
 })
 
