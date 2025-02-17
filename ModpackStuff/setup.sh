babb9bebbde659e6bc0dc9f8bc104879419584b3
WFPhantom <wfphantom5@yahoo.com>
2025-02-17 04:49:41 +0200

    Removed Universal Sawmill, Structory, Smarter Farmers and Dark Mode Everywhere, readded Another Furniture for testing, removed most of explorify structures, Updates, updated InstanceSync to my version

--- ModpackStuff/setup.sh
+++ ModpackStuff/setup.sh
@@ -1,8 +1,9 @@
 cd ..
+MODLIST=modlist.json
+
 echo "#!/bin/sh" > .git/hooks/post-merge
 echo "java -jar InstanceSync.jar" >> .git/hooks/post-merge
 
 echo "Done setting up hooks"
-echo "Running InstanceSync"
-
+java -cp InstanceSync.jar wfphantom.instancesync.ModlistUpdater $MODLIST
 java -jar InstanceSync.jar
