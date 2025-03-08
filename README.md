b0d430216c3d2284715944038dc674ff65df324b
WFPhantom <wfphantom5@gmail.com>
2025-03-08 14:58:27 +0200

    a lot of changes pt2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Neoforge: 21.1.128
+
 Issue tracker and source for [Balanced Eternal](https://www.curseforge.com/minecraft/modpacks/balanced-eternal), a minecraft modpack. im too lazy for a logo or description.
 
 The 1.21.1 branch is heavily WIP and does not work as intended. If you want to test it out feel free to download it and use InstanceSync. Please report any issues you encounter after making sure they are not listed on the [Known Issues](https://github.com/WFPhantom/Balanced-Eternal/issues/1) or [Project Board](https://github.com/users/WFPhantom/projects/4)
--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -5,43 +5,10 @@ ServerEvents.recipes(event => {
     // event.remove({input: 'minecraft:crafter'}),
     event.remove({output: 'minecraft:spyglass'}),
 
-    // AQUACULTURE
-    event.remove({output: 'aquaculture:neptunium_helmet'}),
-    event.remove({output: 'aquaculture:neptunium_chestplate'}),
-    event.remove({output: 'aquaculture:neptunium_leggings'}),
-    event.remove({output: 'aquaculture:neptunium_boots'}),
-    event.remove({output: 'aquaculture:neptunium_pickaxe'}),
-    event.remove({output: 'aquaculture:neptunium_axe'}),
-    event.remove({output: 'aquaculture:neptunium_shovel'}),
-    event.remove({output: 'aquaculture:neptunium_sword'}),
-    event.remove({output: 'aquaculture:neptunium_bow'}),
-    event.remove({output: 'aquaculture:neptunium_hoe'}),
-    event.remove({output: 'aquaculture:neptunes_bounty'}),
-
     // REDSTONE PEN
     event.remove({output: 'redstonepen:remote'}),
     event.remove({output: 'redstonepen:basic_gauge'}),
 
     // COMPUTERCRAFT
-    event.remove({output: 'computercraft:computer_command'}),
-
-    // AE2
-    event.remove({output: 'ae2:meteorite_compass'}),
-    event.remove({output: 'ae2:guide'}),
-    event.remove({output: 'ae2:matter_cannon'}),
-    event.remove({output: 'ae2:portable_item_cell_1k'}),
-    event.remove({output: 'ae2:portable_item_cell_4k'}),
-    event.remove({output: 'ae2:portable_item_cell_16k'}),
-    event.remove({output: 'ae2:portable_item_cell_64k'}),
-    event.remove({output: 'ae2:portable_item_cell_256k'}),
-    event.remove({output: 'ae2:portable_fluid_cell_1k'}),
-    event.remove({output: 'ae2:portable_fluid_cell_4k'}),
-    event.remove({output: 'ae2:portable_fluid_cell_16k'}),
-    event.remove({output: 'ae2:portable_fluid_cell_64k'}),
-    event.remove({output: 'ae2:portable_fluid_cell_256k'})
+    event.remove({output: 'computercraft:computer_command'})
 })
-
-
-ServerEvents.tags("worldgen/biome", (event) => {
-    event.get("ae2:has_meteorites").removeAll()
-  })
