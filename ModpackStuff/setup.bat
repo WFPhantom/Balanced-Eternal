efe9e03ee1f21498fde0cccd272f137e94018116
WFPhantom <wfphantom5@gmail.com>
2026-02-21 20:24:03 +0200

    update PISS to 1.2.0

--- ModpackStuff/setup.bat
+++ ModpackStuff/setup.bat
@@ -1,14 +1,13 @@
 @echo off
 
 cd ..
-set MODLIST=modlist.json
 
 type NUL > .git/hooks/post-merge
 echo #!/bin/sh > .git/hooks/post-merge
 echo java -jar InstanceSync.jar >> .git/hooks/post-merge
 
 echo Done setting up hooks
-
+:: For possible arguments, read https://github.com/WFPhantom/PrismInstanceSync#args
 java -jar InstanceSync.jar
 
 pause
--- ModpackStuff/setup.sh
+++ ModpackStuff/setup.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 cd ..
-MODLIST=modlist.json
 
 echo "#!/bin/sh" > .git/hooks/post-merge
 echo "java -jar InstanceSync.jar" >> .git/hooks/post-merge
 
 echo "Done setting up hooks"
 
+# For possible arguments, read https://github.com/WFPhantom/PrismInstanceSync#args
 java -jar InstanceSync.jar
+
+read -n 1 -s -r -p "Press any key to continue..."
+echo
